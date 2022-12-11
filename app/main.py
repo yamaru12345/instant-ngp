2bc72a9e476de7a6f465d688bc754b0d936077db
yamaru <>
2022-12-12 06:58:43 +0900

    uploaded

--- app/main.py
+++ app/main.py
@@ -11,12 +11,12 @@ def index():
 @app.route("/nerf", methods=['POST'])
 def process():
     images_dir_name = request.get_data()
-    #subprocess.run(['python3', 'scripts/colmap2nerf.py',
-    #                '--run_colmap',
-    #                '--colmap_matcher exhaustive',
-    #                f'--images {images_dir_name}/images',
-    #                '--aabb_scale 1',
-    #                f'--out {images_dir_name}/transforms.json'])
+    subprocess.run(['python3', 'scripts/colmap2nerf.py',
+                    '--run_colmap',
+                    '--colmap_matcher exhaustive',
+                    f'--images {images_dir_name}/images',
+                    '--aabb_scale 1',
+                    f'--out {images_dir_name}/transforms.json'])
 
     return images_dir_name
 
