a0ec109eceed7b2d87a4bcc0870674320ac68867
yamaru <>
2022-12-14 07:25:02 +0900

    uploaded

--- app/main.py
+++ app/main.py
@@ -19,20 +19,20 @@ def index():
 @app.route("/nerf", methods=['POST'])
 def process():
     base_dir = request.get_data()
-    images_dir = os.path.join('/home/data/', str(base_dir, 'utf-8'), 'images')
-    output_json_path = os.path.join('/home/data/', str(base_dir, 'utf-8'), 'transforms.json')
+    images_dir = os.path.join('/home/data', str(base_dir, 'utf-8'), 'images')
+    output_json_path = os.path.join('/home/data', str(base_dir, 'utf-8'), 'transforms.json')
     print('processing colmap estimating...')
     cp = subprocess.run(['python3', 'scripts/colmap2nerf.py',
                          '--run_colmap',
                          '--colmap_matcher', 'exhaustive',
                          '--images', images_dir,
-                         '--aabb_scale', '1',
+                         '--aabb_scale', '2',
                          '--out', output_json_path])
     if cp.returncode != 0:
         return f'ERROR: {cp.returncode}\n'
     else:
         shutil.copy(output_json_path, './transforms.json')
-        output_ply_path = os.path.join('/home/data/', str(base_dir, 'utf-8'), 'mesh.ply')
+        output_ply_path = os.path.join('/home/data', str(base_dir, 'utf-8'), 'mesh.ply')
         print('processing nerf training...')
         cp = subprocess.run(['python3', 'scripts/run.py',
                          '--mode', 'nerf',
