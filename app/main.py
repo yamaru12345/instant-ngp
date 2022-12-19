001fdc535581ed37743b3103ce2d78b61f0d48bb
yamaru <>
2022-12-19 09:38:54 +0900

    uploaded

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ def process():
     shutil.copy(output_json_path, './transforms.json')
     output_ply_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), 'mesh.ply')
     video_camera_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), 'trajectory.json')
-    for i in [0.0, 0.25, 0.5, 0.75, 1.0]:
+    for i in [0.0, 0.25, 0.5, 0.75,1.0]:
         output_video_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), f'video_sharpen_{i}.mp4')
         log_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), f'result_sharpen_{i}.log')
         cp = subprocess.run(['python3', 'scripts/run.py',
@@ -51,8 +51,9 @@ def process():
                             '--video_camera_path', video_camera_path,
                             '--video_n_seconds', '1',
                             '--width', '1280', '--height', '720',
-                            '--video_output', output_video_path,
-                            '>', log_path])
+                            '--video_output', output_video_path])
+        with open(log_path, 'w') as f:
+            f.write(cp.stdout)
     os.remove('./transforms.json')
     if cp.returncode != 0:
         return f'ERROR: {cp.returncode}\n'
