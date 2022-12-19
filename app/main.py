5a6ddad832ae365564bb21faa567c2a6b14a19b7
yamazakitomoya123 <yamazaki.tomoya@robocip.or.jp>
2022-12-19 19:14:44 +0900

    uploaded

--- app/main.py
+++ app/main.py
@@ -39,7 +39,8 @@ def process():
     shutil.copy(output_json_path, './transforms.json')
     output_ply_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), 'mesh.ply')
     video_camera_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), 'trajectory.json')
-    for i in [0.0, 0.25, 0.5, 0.75,1.0]:
+    #for i in [0.0, 0.25, 0.5, 0.75,1.0]:
+    for i in [0.0]
         output_video_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), f'video_sharpen_{i}.mp4')
         log_path = os.path.join('/home/data/output', str(base_dir, 'utf-8'), f'result_sharpen_{i}.log')
         cp = subprocess.run(['python3', 'scripts/run.py',
@@ -53,8 +54,8 @@ def process():
                             '--width', '1280', '--height', '720',
                             '--video_output', output_video_path],
                             capture_output=True, text=True)
-        with open(log_path, 'w') as f:
-            f.write(cp.stdout)
+        #with open(log_path, 'w') as f:
+        #    f.write(cp.stdout)
     os.remove('./transforms.json')
     if cp.returncode != 0:
         return f'ERROR: {cp.returncode}\n'
